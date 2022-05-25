275338fb1c136cd560e2c24ba122e6b1f3c2b345
RaitoRyuWorker <longnguyen58162001@gmail.com>
2022-05-25 15:36:55 +0700

    commit 2

--- web_core/urls.py
+++ web_core/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('login', views.loginPage, name='login'),
     path('logout', views.logoutUser, name='logout'),
     path('dskb', views.dskb, name='quanli_dskb'),
-    path('dsbn', views.dsbn, name='quanli_dsbn'),
     path('dspk',views.dspk, name='quanli_dspk'),
 ]
